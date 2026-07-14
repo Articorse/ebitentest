3f8db080ec29c1e8427dc49bef97aac60677f5c9
Chavdar Zhechev <chavdar.zhechev@gmail.com>
2026-07-15 00:26:47 +0900

    [Constants] Add MaxChunkLoadDispatchPerTick

--- data/constants.go
+++ data/constants.go
@@ -5,26 +5,27 @@ import (
 )
 
 const (
-	TPS                              = 60
-	CameraWidth                      = 640
-	CameraHeight                     = 360
-	VelocityThreshold                = 0.01
-	DefaultDrag                      = 0.8
-	DefaultAcceleration              = 1
-	SpatialHashGridCellSize          = 200
-	Bounciness                       = 0.0
-	AABBPadding                      = 20
-	TickMs                           = 1000 / TPS
-	MaxAbilitySlots                  = 16
-	MaxEquipmentAbilitySlots         = 2
-	GamepadDeadzone                  = 0.15
-	GamepadAimDeadzone               = 0.5
-	TileSize                         = 16
-	ChunkSize                        = 4
-	ChunkDataFilePathTemplate        = "chunkdata/chunk_%d_%d.gob"
-	ChunkReloadRetries               = 3
-	RngSeed1                  uint64 = 0b1111110001111110011111000111000010001001001111111000101110001110
-	RngSeed2                  uint64 = 0b0101100110101000111000010000011001000001011000011001100101101001
+	TPS                                = 60
+	CameraWidth                        = 640
+	CameraHeight                       = 360
+	VelocityThreshold                  = 0.01
+	DefaultDrag                        = 0.8
+	DefaultAcceleration                = 1
+	SpatialHashGridCellSize            = 200
+	Bounciness                         = 0.0
+	AABBPadding                        = 20
+	TickMs                             = 1000 / TPS
+	MaxAbilitySlots                    = 16
+	MaxEquipmentAbilitySlots           = 2
+	GamepadDeadzone                    = 0.15
+	GamepadAimDeadzone                 = 0.5
+	TileSize                           = 16
+	ChunkSize                          = 4
+	ChunkDataFilePathTemplate          = "chunkdata/chunk_%d_%d.gob"
+	ChunkReloadRetries                 = 3
+	MaxChunkLoadDispatchPerTick        = 4
+	RngSeed1                    uint64 = 0b1111110001111110011111000111000010001001001111111000101110001110
+	RngSeed2                    uint64 = 0b0101100110101000111000010000011001000001011000011001100101101001
 )
 
 var (
